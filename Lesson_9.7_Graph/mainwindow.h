651b670a6e4a06c9da3fc0168822a58f1cf3419d
Artem Gyulumyan <timon9393@mail.ru>
2024-08-21 21:42:50 +0300

    add DZ 9.7_Graph_customPlot

---
+++ Lesson_9.7_Graph/mainwindow.h
@@ -0,0 +1,67 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QDataStream>
+#include <QMessageBox>
+#include <QtGlobal>
+#include <QtConcurrent>
+#include <algorithm>
+#include "qcustomplot.h"
+#include <QVector>
+
+///Подключаем все что нужно для графиков
+//#include <QLineSeries>
+//#include <QtCharts>
+//#include <QChartView>
+
+#define FD 1000.0 //частота дискретизации
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class QCustomPlot;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    //Метод считывает данные из исходного файла
+    QVector<uint32_t> ReadFile(QString path, uint8_t numberChannel);
+    //Метод преобразует данные физические величины, В?
+    QVector<double> ProcessFile(QVector<uint32_t> dataFile);
+    //Метод ищет Максимумы
+    QVector<double> FindMax(QVector<double> resultData);
+    //Метод ищет минимумы
+    QVector<double> FindMin(QVector<double> resultData);
+    //Метод отображает результаты
+    void DisplayResult(QVector<double> mins, QVector<double> maxs);
+
+
+
+private slots:
+    void on_pb_path_clicked();
+    void on_pb_start_clicked();
+    void plotData(QCustomPlot *customPlot, const QVector<double> &data);
+
+signals:
+    void dataReady(QCustomPlot *customPlot, const QVector<double> &data);
+
+
+private:
+    Ui::MainWindow *ui;
+    QString pathToFile = "";
+    uint8_t numberSelectChannel = 0xEA;
+    QCustomPlot *customPlot;
+    QMainWindow *plotWindow;
+    QVector<uint32_t> readData;
+    QVector<double> procesData;
+    QVector<double> mins, maxs;
+
+};
+#endif // MAINWINDOW_H
